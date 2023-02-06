b362a4dee8efed3302dafb4fc4319ba6764ee6af
sppunderreview <102622860+sppunderreview@users.noreply.github.com>
2023-02-06 15:28:15 +0100

    Update ComputePreprocessingTimesPSSO.py

--- Windows/Redaction/ComputePreprocessingTimesPSSO.py
+++ Windows/Redaction/ComputePreprocessingTimesPSSO.py
@@ -4,7 +4,7 @@ import pickle
 with open("preprocessQPSS16", "rb") as f:
 	preprocessQPSS = pickle.load(f)
 
-for nQBI in [ "QB","QB40k", "QB20K"]:
+for nQBI in [ "QB"]:
     with open(nQBI, "rb") as f:
         E = pickle.load(f) # [Q,B,I,QNames]
         Q = E[0]    
@@ -16,6 +16,4 @@ for nQBI in [ "QB","QB40k", "QB20K"]:
 
 """
 QB    0.39305151153939477 19430.50147294998
-QB40k 0.3828675242647826 9563.26502108574
-QB20K 0.38622554943261556 4665.604637145996
 """
