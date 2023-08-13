5d12d5cec4f7166f6f17a87cb6feaada79f0812a
Tristan BENOIT <tristan.benoit@loria.fr>
2023-08-13 20:13:34 +0200

    c

--- AlphaDiff/Embeds/MakeEmbeds.py
+++ AlphaDiff/Embeds/MakeEmbeds.py
@@ -18,12 +18,12 @@ def run(OA, OB, folder):
     
 if __name__ == '__main__':
     import sys
-    sys.path.insert(0, "????") # PSS_PATH_BASIC_CV 
-    sys.path.insert(0, "????") # PSS_PATH_BASIC_CO 
-    sys.path.insert(0, "????") # PSS_PATH_BASIC_UV 
-    sys.path.insert(0, "????") # PSS_PATH_BASIC_UO 
-    sys.path.insert(0, "????") # PSS_PATH_BASIC_BV 
-    sys.path.insert(0, "????") # PSS_PATH_BASIC_BO 
+    sys.path.insert(0, "???") # PSS_PATH_BASIC_CV 
+    sys.path.insert(0, "???") # PSS_PATH_BASIC_CO 
+    sys.path.insert(0, "???") # PSS_PATH_BASIC_UV 
+    sys.path.insert(0, "???") # PSS_PATH_BASIC_UO 
+    sys.path.insert(0, "???") # PSS_PATH_BASIC_BV 
+    sys.path.insert(0, "???") # PSS_PATH_BASIC_BO 
 
     from makeBenchBO import benchmarkBO
     from makeBenchBV import benchmarkBV
@@ -33,8 +33,8 @@ if __name__ == '__main__':
     from makeBenchCO import benchmarkCO
     
     run(benchmarkCV("V0","V1"),benchmarkCV("V2","V3"),"CV")
-    #run(benchmarkCO("O0","O1"),benchmarkCO("O2","O3"),"CO")    
-    #run(benchmarkUV("V0","V1"),benchmarkUV("V2","V3"),"UV")
-    #run(benchmarkUO("O0","O1"),benchmarkUO("O2","O3"),"UO")    
-    #run(benchmarkBV("V0","V1"),benchmarkBV("V2","V3"),"BV")
-    #run(benchmarkBO("O0","O1"),benchmarkBO("O2","O3"),"BO")
+    run(benchmarkCO("O0","O1"),benchmarkCO("O2","O3"),"CO")    
+    run(benchmarkUV("V0","V1"),benchmarkUV("V2","V3"),"UV")
+    run(benchmarkUO("O0","O1"),benchmarkUO("O2","O3"),"UO")    
+    run(benchmarkBV("V0","V1"),benchmarkBV("V2","V3"),"BV")
+    run(benchmarkBO("O0","O1"),benchmarkBO("O2","O3"),"BO")
