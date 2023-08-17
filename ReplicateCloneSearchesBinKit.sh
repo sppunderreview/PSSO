cd560e884acd7e1927650e33d529dd452ed5df90
Tristan BENOIT <tristan.benoit@loria.fr>
2023-08-17 15:50:09 +0200

    Scripts to reproduce Basic and BinKiT Clone Searches

--- ReplicateCloneSearchesBinKit.sh
+++ ReplicateCloneSearchesBinKit.sh
@@ -1,6 +1,6 @@
 # Replicate clone searches, without the preprocssing step, for the BinKit Dataset
 
-echo 'BinKiT dataset clone searches'
+echo 'BinKiT dataset clone searches, without the preprocssing step'
 echo 'Takes around 3150 hours roughly divided by the number of cores used (40), thus between 140 hours and 240 hours'
 
 cd BinKit/
